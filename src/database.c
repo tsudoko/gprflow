2caa61b4e290653832b1bc1dd18a34504a56929a
flan <flan@flande.re>
2018-03-06 18:38:59 +0100

    Implement rest of database parser

--- src/database.c
+++ src/database.c
@@ -3,18 +3,36 @@
 #include <stdlib.h>
 
 #include "reader.h"
+#include "database.h"
+
+static const int OFFINT = 0x3e8;
+static const int OFFSTR = 0x7d0;
+static const unsigned char DATMAGIC[] = "W\0\0OL\0FM\0\xc1";
+
+void
+type_countvalues(Type *t, int *nint, int *nstr)
+{
+	nint = 0, nstr = 0;
+	for(int i = 0; i < t->nfield; i++)
+		if(t->fields[i].offset >= OFFSTR)
+			nstr++;
+		else
+			nint++;
+}
 
 void
 type_loadproject(Reader *r, Type *t)
 {
 	t->name = readstr(r);
 	t->nfield = readint(r);
+	printf("project nfield: %d\n", t->nfield);
 
 	t->fields = malloc(sizeof *t->fields * t->nfield);
 	for(int i = 0; i < t->nfield; i++)
 		t->fields[i].name = readstr(r);
 
 	t->ndata = readint(r);
+	printf("project ndata: %d\n", t->ndata);
 	for(int i = 0; i < t->ndata; i++)
 		t->data[i].name = readstr(r);
 
@@ -23,9 +41,9 @@ type_loadproject(Reader *r, Type *t)
 	int n = readint(r), i;
 	printf("field list size: %d\n", n);
 	assert(n >= t->nfield);
-	for(i = 0; i < nfield; i++)
+	for(i = 0; i < n; i++)
 		t->fields[i].type = readbyte(r);
-	printf("real field list end: 0x%x\n", ftell(f));
+	printf("real field list end: 0x%x\n", ftell(r->f));
 	fseek(r->f, n-i, SEEK_CUR);
 
 	n = readint(r);
@@ -45,7 +63,7 @@ type_loadproject(Reader *r, Type *t)
 
 	n = readint(r);
 	assert(n >= t->nfield);
-	for(i = 0; i < n; i++)
+	for(i = 0; i < n; i++) {
 		Field *f = &t->fields[i];
 		f->narg = readint(r);
 		f->args = malloc(sizeof *f->args * f->narg);
@@ -56,13 +74,39 @@ type_loadproject(Reader *r, Type *t)
 	n = readint(r);
 	assert(n >= t->nfield);
 	for(i = 0; i < n; i++)
-		t->fields[i].default = readint(r);
+		t->fields[i].default_ = readint(r);
 }
 
 void
 type_loaddat(Reader *r, Type *t)
 {
-	TODO;
+	if(readncmp(r, (unsigned char *)"\xfe\xff\xff\xff", 4) != 0)
+		printf("unexpected dat separator: \\x%x\\x%x\\x%x\\x%x\n", r->buf[0], r->buf[1], r->buf[2], r->buf[4]);
+
+	t->something = readint(r);
+	int nfield = readint(r);
+	printf("dat nfield: %d\n", nfield);
+	assert(nfield <= t->nfield);
+	for(int i = 0; i < nfield; i++)
+		t->fields[i].offset = readint(r);
+
+	int ndata = readint(r), nint, nstr;
+	printf("dat ndata: %d\n", ndata);
+	assert(ndata <= t->ndata);
+
+	type_countvalues(t, &nint, &nstr);
+
+	for(int i = 0; i < ndata; i++) {
+		t->data[i].nint = nint;
+		t->data[i].ints = malloc(sizeof *t->data[i].ints * nint);
+		for(int j = 0; j < t->data[i].nint; j++)
+			t->data[i].ints[j] = readint(r);
+
+		t->data[i].nstr = nstr;
+		t->data[i].strs = malloc(sizeof *t->data[i].strs * nstr);
+		for(int j = 0; j < t->data[i].nstr; j++)
+			t->data[i].strs[j] = readstr(r);
+	}
 }
 
 void
@@ -91,15 +135,27 @@ database_load(char *projectpath, char *datpath)
 	Database *d = malloc(sizeof *d);
 
 	r.f = fopen(projectpath, "rb");
-	d->n = readint(r);
+	d->n = readint(&r);
 	d->t = malloc(d->n * sizeof (int));
 	for(int i = 0; i < d->n; i++)
 		type_loadproject(&r, d->t + i);
 	fclose(r.f);
 
 	r.f = fopen(datpath, "rb");
+	assert(readbyte(&r) == 0); /* encrypted if !0 */
+	if(readncmp(&r, DATMAGIC, sizeof DATMAGIC) != 0) {
+		printf("unexpected dat magic\n");
+		return NULL;
+	}
+
+	int dn = readint(&r);
+	assert(dn == d->n);
+
 	for(int i = 0; i < d->n; i++)
 		type_loaddat(&r, d->t + i);
+
+	if(readbyte(&r) != '\xc1')
+		printf("unexpected dat terminator: \\x%x\n", r.buf[0]);
 	fclose(r.f);
 
 	return d;
--- src/database.h
+++ src/database.h
@@ -1,27 +1,34 @@
 typedef struct {
 	unsigned char *name;
+	unsigned char type;
+	unsigned char *something;
+	int nstrarg;
+	unsigned char **strargs;
+	int narg;
+	int *args;
+	int default_;
+	int offset;
+} Field;
+
+typedef struct {
+	unsigned char *name;
+	int nint;
+	int *ints;
+	int nstr;
+	unsigned char **strs;
+} Typedata;
+
+typedef struct {
+	unsigned char *name;
 	int nfield;
 	Field *fields;
+	int something;
 	int ndata;
 	Typedata *data;
 	unsigned char *desc;
 } Type;
 
 typedef struct {
-	unsigned char *name; /* str */
-	unsigned char type;
-	unsigned char *something; /* str */
-	int nstrarg;
-	unsigned char **strargs; /* *str */
-	int narg;
-	int *args;
-	int default;
-} Field;
-
-typedef struct {
-	unsigned char *name; /* str, from the project file */
-	int nint;
-	int *ints;
-	int nstr;
-	unsigned char **strs;
-} Typedata;
+	int n;
+	Type *t;
+} Database;
